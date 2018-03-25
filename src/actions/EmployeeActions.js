45e1fddd66efcda84b1b88f622342370f2803cda
Sourabh Berde <sourabh.berde44@gmail.com>
2018-03-25 12:29:57 +0530

    Upto Touchable List

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -19,7 +20,20 @@ export const employeeCreate = ({ name, phone, shift }) => {
     .push({ name, phone, shift })
     .then(() => {
       dispatch({ type: EMPLOYEE_CREATE });
-      Actions.pop()
+      Actions.pop();
     });
 };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/empoyees`)
+      .on('value', snapshot => {
+        dispatch({
+          type: EMPLOYEES_FETCH_SUCCESS,
+          payload: snapshot.val()
+        });
+      });
+  };
 };
